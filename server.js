4b078d36df735dee8f63f31e25716d370f97fad3
JohnFarmer1945 <john.farmer@gmx.net>
2023-12-05 08:28:49 +0100

    First attempt cors added with '+'

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const whiteboardRoutes = require("./routes/whiteboard");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // express app
 const app = express();
@@ -14,6 +15,13 @@ app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 // routes
 app.use("/api/whiteboard", whiteboardRoutes);
 
