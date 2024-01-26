9792294ef8be8fb60036606fb941d87f76bdc41c
JohnFarmer1945 <john.farmer@gmx.net>
2024-01-26 08:32:48 +0100

    d

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: [
-      "https://drf-crewbriefing.netlify.app",
-      "https://drf-crewbriefing.netlify.app/",
-    ],
+    origin: "https://drf-crewbriefing.netlify.app",
   })
 );
 
