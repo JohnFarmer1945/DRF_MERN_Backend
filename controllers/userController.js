8771808b651d2211574e99f8e89e8916f1ffe33d
JohnFarmer1945 <john.farmer@gmx.net>
2024-02-07 13:38:58 +0100

    user changed from email to username

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,15 +15,15 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
   try {
-    const user = await User.login(email, password);
+    const user = await User.login(username, password);
 
     // create token
     const token = createToken(user._id);
 
-    res.status(200).json({ email, token });
+    res.status(200).json({ username, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -31,15 +31,15 @@ const loginUser = async (req, res) => {
 
 // signup user
 const singnupUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
   try {
-    const user = await User.signup(email, password);
+    const user = await User.signup(username, password);
 
     // create token
     const token = createToken(user._id);
 
-    res.status(200).json({ email, token });
+    res.status(200).json({ username, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: {
+  username: {
     type: String,
     required: true,
     unique: true,
@@ -17,17 +17,13 @@ const userSchema = new Schema({
 });
 
 // static signup method
-userSchema.statics.signup = async function (email, password) {
-  const exists = await this.findOne({ email });
+userSchema.statics.signup = async function (username, password) {
+  const exists = await this.findOne({ username });
 
   // validation
 
-  if (!email || !password) {
+  if (!username || !password) {
     throw Error("All fields must be filled.");
-  }
-
-  if (!validator.isEmail(email)) {
-    throw Error("Email is not valid.");
   }
 
   if (!validator.isStrongPassword(password)) {
@@ -35,29 +31,29 @@ userSchema.statics.signup = async function (email, password) {
   }
 
   if (exists) {
-    throw Error("Email already in use");
+    throw Error("Username already in use");
   }
 
   const salt = await bcrypt.genSalt(10);
 
   const hash = await bcrypt.hash(password, salt);
 
-  const user = await this.create({ email, password: hash });
+  const user = await this.create({ username, password: hash });
 
   return user;
 };
 
 // static login method
 
-userSchema.statics.login = async function (email, password) {
-  if (!email || !password) {
+userSchema.statics.login = async function (username, password) {
+  if (!username || !password) {
     throw Error("All fields must be filled.");
   }
 
-  const user = await this.findOne({ email });
+  const user = await this.findOne({ username });
 
   if (!user) {
-    throw Error("Incorrect email.");
+    throw Error("Incorrect username.");
   }
 
   const match = await bcrypt.compare(password, user.password);
